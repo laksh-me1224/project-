9b8754beaa9a27cfa11b942417cdc6d8f54aec78
laksh-me1224 <yoga.lsen@gmail.com>
2022-12-06 23:07:38 +0400

    another commit

---
+++ cowbulls.py
@@ -0,0 +1,40 @@
+import random
+
+def compare_numbers(number, user_guess):
+    cow=0
+    bull=0
+    for j in range (0,4):
+        if number[j]==user_guess[j]:
+            bull=bull+1
+        else:
+            cow=cow+1
+        
+    ## your code here
+    return cow,bull
+
+playing = True #gotta play the game
+number = str(random.randint(0,9999)) #random 4 digit number
+guesses = 0
+print ("number")
+
+print("Let's play a game of Cowbull!") #explanation
+print("I will generate a number, and you have to guess the numbers one digit at a time.")
+print("For every number that exists in the sequence but is in wrong place, you get a cow. For every one in the right place, you get a bull.")
+print("The game ends when you get 4 bulls!")
+print("Type exit at any prompt to exit.")
+
+while playing:
+    user_guess =input("Give me your best guess!")
+    if user_guess == "exit":
+        break
+    cowbullcount = compare_numbers(number,user_guess)
+    guesses+=1
+
+    print("You have "+ str(cowbullcount[0]) + " cows, and " + str(cowbullcount[1]) + " bulls.")
+
+    if cowbullcount[1]==4:
+        playing = False
+        print("You win the game after " + str(guesses) + "! The number was "+str(number))
+        break #redundant exit
+    else:
+        print("Your guess isn't quite right, try again.")
